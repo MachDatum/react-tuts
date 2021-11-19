964301fb5ec9725636baea0474807359c4923a70
Hemanand Ramasamy <hemanand@machdatum.com>
2021-11-19 17:25:15 +0530

    Demo Tuts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import {DefaultButton, PrimaryButton, TextField,Text} from "@fluentui/react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const defaultUsers = [
+  {name: "Hemanand", gender: "Male", id:1},
+  {name: "Nish", gender: "Male", id:2},
+  {name: "Jagadesh", gender: "Male", id:3},
+  {name: "Nandhana", gender: "Female", id:4},
+  {name: "Sobiya", gender: "Female", id:5},
+]
+
+export default function App(){
+  const [users, setUsers] = useState(defaultUsers);
+
+  useEffect(() => {
+    console.log("Welcome to the application");
+  }, []);
+
+  useEffect(() => {
+    console.log("Users were modified")
+  }, [users])
+
+  return(
+    <div className="container">
+      {
+        users.map((user) => 
+        <Hello 
+          key={user.id} 
+          id={user.id}
+          name={user.name} 
+          gender={user.gender} 
+          onDelete={handleDelete}
+          onAction={takeAction} />)
+      }
     </div>
-  );
+  )
+
+  function takeAction(name){
+    console.log("Action Taken for " + name);
+  }
+
+  function handleDelete(id){
+    setUsers(users.filter(user => user.id !== id));
+  }
 }
 
-export default App;
+function Hello(props){
+  const [value, setValue] = useState(props.name);
+
+  const isNameShorterThan6 = value.length < 6;
+
+  return(
+    <div className={"user"}>
+      <Text>Hello </Text>
+      <TextField 
+        value={value} 
+        onChange={(event) => setValue(event.target.value)}>
+      </TextField>
+      <PrimaryButton  disabled={isNameShorterThan6} onClick={handleAction}>Confirm</PrimaryButton>
+      <DefaultButton onClick={() => props.onDelete(props.id)}>Delete</DefaultButton>
+    </div>
+  )
+
+  function handleAction(){
+    props.onAction(value);
+  }
+}
